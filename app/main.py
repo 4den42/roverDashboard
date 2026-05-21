d3b2659d8f1829205ba515814a6651a888da4fc3
4den42 <hartmanaden@gmail.com>
2026-05-20 19:43:11 -0700

    we'll see

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.telemetry import telemetry_generator
 from fastapi.responses import StreamingResponse
-from app.core.camera import latest_frame
+import app.core.camera as camera_module
 import asyncio
 
 app = FastAPI()
@@ -22,13 +22,12 @@ async def telemetry_ws(websocket: WebSocket):
         await websocket.send_json(data)
 
 async def generate():
-    global latest_frame
     while True:
-        if latest_frame is None:
+        if camera_module.latest_frame is None:
             await asyncio.sleep(0.03)
             continue
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + latest_frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + camera_module.latest_frame + b'\r\n')
         await asyncio.sleep(0.03)
 
 @app.get("/camera")
