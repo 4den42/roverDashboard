9cbce54897014baf1cecaa3139fa5e6a6cbfd95b
4den42 <hartmanaden@gmail.com>
2026-05-20 19:34:05 -0700

    hopefully the cam works

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.telemetry import telemetry_generator
 from fastapi.responses import StreamingResponse
 from app.core.camera import latest_frame
-import time
+import asyncio
 
 app = FastAPI()
 
@@ -18,22 +18,18 @@ app.add_middleware(
 @app.websocket("/ws/telemetry")
 async def telemetry_ws(websocket: WebSocket):
     await websocket.accept()
-
     async for data in telemetry_generator():
         await websocket.send_json(data)
 
-def generate():
+async def generate():
     global latest_frame
-
     while True:
         if latest_frame is None:
+            await asyncio.sleep(0.03)
             continue
-
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + latest_frame + b'\r\n')
-
-        time.sleep(0.03)
-
+        await asyncio.sleep(0.03)
 
 @app.get("/camera")
 def camera_feed():
